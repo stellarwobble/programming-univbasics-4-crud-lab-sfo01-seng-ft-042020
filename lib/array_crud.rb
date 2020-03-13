c0311c605eef14f88af684a7079461f6a7acef19
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-13 00:48:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -48,5 +48,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+ array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    index_number = 4
+    
 end
