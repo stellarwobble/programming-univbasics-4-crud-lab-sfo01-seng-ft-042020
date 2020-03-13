3a6cbf56ed7cbbab0aa87732a4e9a67f09b43612
Jennifer Paquette <jennidemuir@gmail.com>
2020-03-13 00:49:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -51,4 +51,5 @@ def update_element_from_index(array, index_number, element)
  array = ["wow", "I", "am", "really", "learning", "arrays!"]
     index_number = 4
     element = "totally"
+    array[index_number] = element
 end
